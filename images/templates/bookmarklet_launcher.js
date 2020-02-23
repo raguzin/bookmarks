f68ab20336e819212be10efab3badd0c2501b581
a.raguzin <a.raguzin83@gmail.com>
2020-02-23 18:35:25 +0300

    Create bookmarklet

---
+++ images/templates/bookmarklet_launcher.js
@@ -0,0 +1,11 @@
+(function () {
+    if (window.myBookmarklet !== undefined) {
+        myBookmarklet();
+    }
+    else {
+        document.body.appendChild(
+            document.createElement('script')
+        ).src = 'https://e72861bc.ngrok.io/static/js/bookmarklet.js?r=' +
+            Math.floor(Math.random() * 99999999999999999999);
+    }
+})();
